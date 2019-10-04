34140e26a280d0732eacdee6b66e7ccbb42dd200
Brandon Lee <j474lee@edu.uwaterloo.ca>
2019-10-04 15:53:24 -0400

    keeping track of evaluation time

--- experiments/BERT/heads_ablation.sh
+++ experiments/BERT/heads_ablation.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# keep newline when we get output from a command
+# https://unix.stackexchange.com/questions/164508/why-do-newline-characters-get-lost-when-using-command-substitution
+IFS=
+
 TASK=$1
 OPTIONS="${@:2}"
 LOG_FILE="models/${TASK}/ablation_results.txt"
@@ -18,20 +22,31 @@ echo $base_acc >> $LOG_FILE
 
 echo "=== evaluate ablation ==="
 
+exp_start_time=$(date +%s.%N)
+
 for layer in `seq 1 12`
 do
     echo "layer - $layer"
     for head in `seq 1 12`
     do
         mask_str="${layer}:${head}"
-        echo "head - $head, mask_str - $mask_str"
+        echo "$mask_str $OPTIONS"
+
+        start_time=$(date +%s.%N)
         acc=$(run_eval "--attention_mask_heads $mask_str $OPTIONS" | grep $metric | rev | cut -d" " -f1 | rev)
+        end_time=$(date +%s.%N)
+        echo "\ttime elapsed - " $(echo "$end_time - $start_time" | bc)
+
         echo -en "\tacc - $acc"
         printf "\t diff - %.5f\n" $(echo "$acc - $base_acc" | bc)
 
         echo "$acc" >> $LOG_FILE
     done
 done
+
+exp_end_time=$(date +%s.%N)
+
+echo "experiment time elapsed - " $(echo "$exp_end_time - $exp_start_time" | bc)
 
 
 
