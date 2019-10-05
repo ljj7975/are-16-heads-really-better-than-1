2cc2399cc8d1398fa7c97fff285308f5b9a8e9f0
Brandon Lee <j474lee@edu.uwaterloo.ca>
2019-10-04 20:30:47 -0400

    script for comb 2

--- experiments/BERT/heads_ablation.sh
+++ experiments/BERT/heads_ablation.sh
@@ -7,6 +7,7 @@ LOG_FILE="models/${TASK}/ablation_results.txt"
 if [[ $OPTIONS == *"--reverse_head_mask"* ]]; then
     LOG_FILE="models/${TASK}/reverse_ablation_results.txt"
 fi
+
 echo "log file : $LOG_FILE"
 echo "task : $TASK"
 echo "options : $OPTIONS"
@@ -24,29 +25,38 @@ echo "=== evaluate ablation ==="
 
 exp_start_time=$(date +%s.%N)
 
+
 for layer in `seq 1 12`
 do
     echo "layer - $layer"
-    for head in `seq 1 12`
+    for head_1 in `seq 1 12`
     do
-        mask_str="${layer}:${head}"
-        echo -e "\t$mask_str $OPTIONS"
+        mask_str="${layer}:${head_1},"
+        for head_2 in `seq 1 12`
+        do
 
-        start_time=$(date +%s.%N)
-        acc=$(run_eval "--attention_mask_heads $mask_str $OPTIONS" | grep $metric | rev | cut -d" " -f1 | rev)
-        end_time=$(date +%s.%N)
-        echo -e "\ttime elapsed - " $(echo "$end_time - $start_time" | bc)
+            if [ "${head_1}" == "${head_2}" ]; then
+                continue
+            fi
 
-        echo -en "\tacc - $acc"
-        printf "\t diff - %.5f\n" $(echo "$acc - $base_acc" | bc)
+            mask_str+="${head_2}"
+            echo -e "\t$mask_str $OPTIONS"
 
-        echo "$acc" >> $LOG_FILE
+            start_time=$(date +%s.%N)
+
+            acc=$(run_eval "--attention_mask_heads $mask_str_1 $mask_str_2 $OPTIONS" | grep $metric | rev | cut -d" " -f1 | rev)
+            end_time=$(date +%s.%N)
+            echo -e "\ttime elapsed - " $(echo "$end_time - $start_time" | bc)
+
+            echo -en "\tacc - $acc"
+            printf "\t diff - %.5f\n" $(echo "$acc - $base_acc" | bc)
+
+            echo -e "$mask_str\t$acc" >> $LOG_FILE
+        done
     done
 done
+
 
 exp_end_time=$(date +%s.%N)
 
 echo "experiment time elapsed - " $(echo "$exp_end_time - $exp_start_time" | bc)
-
-
-
