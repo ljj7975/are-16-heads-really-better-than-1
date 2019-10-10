e8c25604269f5d0d9865a52a0280ea962d2673d4
Jaejun Lee <j474lee@beluga2.int.ets1.calculquebec.ca>
2019-10-09 22:09:36 -0400

    scripts

---
+++ scripts/MNLI.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --time=1-00:00:00
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=20G
+
+source ~/ENV/bin/activate
+
+TASK='MNLI'
+
+CUDA_VISIBLE_DEVICES=0 bash experiments/BERT/heads_ablation.sh $TASK &
+sleep 1m
+
+CUDA_VISIBLE_DEVICES=1 bash experiments/BERT/heads_ablation.sh $TASK --reverse_head_mask &
+
+wait
+
+deactivate
+
+
